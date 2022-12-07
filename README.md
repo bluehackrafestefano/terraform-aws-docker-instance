7bd056a31d81f1eff839d88db871882031761638
Rafe <stefanorafe@gmail.com>
2022-12-07 19:11:56 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+Terraform Module to provision an AWS EC2 instance with the latest ubuntu ami and installed docker in it.
+
+Usage:
+
+```hcl
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "docker_instance" {
+    source = "<github-username>/docker-instance/aws"
+    key_name = "task"
+}
---
+++ main.tf
@@ -0,0 +1,68 @@
+data "aws_ami" "ubuntu" {
+  owners      = ["099720109477"] # Canonical
+  most_recent = true
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+}
+
+data "template_file" "userdata" {
+  template = file("${abspath(path.module)}/userdata.sh")
+  vars = {
+    server-name = var.server-name
+  }
+}
+
+resource "aws_instance" "tfmyec2" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = var.instance_type
+  count                  = var.num_of_instance
+  key_name               = var.key_name
+  vpc_security_group_ids = [aws_security_group.tf-sec-gr.id]
+  user_data              = data.template_file.userdata.rendered
+  tags = {
+    Name = var.tag
+  }
+}
+
+resource "aws_security_group" "tf-sec-gr" {
+  name = "${var.tag}-terraform-sec-grp"
+  tags = {
+    Name = var.tag
+  }
+
+  dynamic "ingress" {
+    for_each = var.docker-instance-ports
+    iterator = port
+    content {
+      from_port   = port.value
+      to_port     = port.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+
+  egress {
+    from_port   = 0
+    protocol    = "-1"
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
